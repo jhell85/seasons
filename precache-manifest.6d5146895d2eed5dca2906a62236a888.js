5741c2232c2bc7c4ea5d95594207489371502144
jhell85 <jhell85@gmail.com>
2020-11-15 18:30:55 -0800

    Updates

---
+++ precache-manifest.6d5146895d2eed5dca2906a62236a888.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6422551fb44e19bc71c02f2927716a70",
+    "url": "/seasons/index.html"
+  },
+  {
+    "revision": "20133eeab05d39f34e7b",
+    "url": "/seasons/static/css/main.f386a5ee.chunk.css"
+  },
+  {
+    "revision": "c2d246ecc2b83ccac6a6",
+    "url": "/seasons/static/js/2.8b77c9df.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/seasons/static/js/2.8b77c9df.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "20133eeab05d39f34e7b",
+    "url": "/seasons/static/js/main.6b483a26.chunk.js"
+  },
+  {
+    "revision": "7aa38b62a80534558f86",
+    "url": "/seasons/static/js/runtime-main.53d2d0be.js"
+  }
+]);
